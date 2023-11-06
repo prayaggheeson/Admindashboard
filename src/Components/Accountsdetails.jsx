0aa21135c19cacd0b6e28a88013101356f9cd9ba
prayaggheeson <prayag.gheeson@gmail.com>
2023-11-06 14:49:29 +0530

    updated footer and Components

--- src/Components/Accountsdetails.jsx
+++ src/Components/Accountsdetails.jsx
@@ -30,26 +30,31 @@ const Accountsdetails = () => {
       console.log(error);
     }
   };
+
   return (
     <div className="w-full p-4 bg-white hover:shadow-xl hover:shadow-blue-900 rounded-lg">
-      <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mb-4 sm:mb-0">
-        <h2 className="text-2xl sm:text-3xl pt-5 text-center sm:text-start font-semibold">
+      <div className="flex flex-col sm:flex-row justify-evenly items-center gap-4 mb-4 sm:mb-0">
+        <h2 className="text-2xl sm:text-3xl px-6 py-2 text-center sm:text-start font-semibold">
           Username
         </h2>
-        <button
-          className="bg-blue-300 hover:bg-blue-700 hover:shadow-lg hover:shadow-blue-700 text-white px-6 py-2 rounded-lg mr-4"
-          onClick={connectWallet}
-        >
-          connect Wallet
-        </button>
+        {walletAddress ? (
+          <div className=" text-black  px-6 py-2 rounded-lg mr-4">
+            Wallet Connected
+          </div>
+        ) : (
+          <button
+            className="bg-blue-300 hover:bg-blue-700 hover:shadow-lg hover:shadow-blue-700 text-white px-6 py-2 rounded-lg mr-4"
+            onClick={connectWallet}
+          >
+            Connect Wallet
+          </button>
+        )}
       </div>
 
-      <div className="flex flex-wrap ">
+      <div className="flex flex-wrap">
         <div className="w-full sm:w-1/2 md:w-1/3">
           <div className="p-4 rounded-full justify-center hover:bg-blue-300">
-            <h2 className="text-xl font-semibold">
-              {walletAddress ?` ${walletAddress.substring(0, 15)}...` : "Not Connected"}
-            </h2>
+            <h2 className="text-xl font-semibold">{walletAddress ? walletAddress.substring(0, 15) + "..." : "Not Connected"}</h2>
           </div>
         </div>
         <div className="w-full sm:w-1/2 md:w-1/3 mb-4 sm:mb-0">
